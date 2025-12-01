3bc9e8887758eec3227e9170d87e7c11c264807d
tinnedanchovies <mollyvz009@gmail.com>
2025-12-01 13:17:07 -0800

    Added Levels 1-3, Added Globals script, fixed score

--- godot-apple-dropper/apple2.gd
+++ godot-apple-dropper/apple2.gd
@@ -2,10 +2,8 @@ extends CharacterBody2D
 
 var gravity = 30
 var score = 0
-var score_label
 
 func _ready():
-	score_label = get_node("/root/Node2D/ScoreLabel")
 	velocity = Vector2(0, 200)
 	
 func _physics_process(delta):
@@ -21,5 +19,4 @@ func _on_basket_body_entered(body: Node2D) -> void:
 	position.y = 0
 	position.x = randi_range(0, 1500)
 	velocity.y = 200
-	score = score + 1
-	score_label.text = str(score)
+	Globals.score = Globals.score + 1
--- godot-apple-dropper/score_label.gd
+++ godot-apple-dropper/score_label.gd
@@ -1,1 +1,10 @@
-extends Label
+extends Control
+
+var score = 0
+
+func _process(delta: float) -> void:
+	$score.text = "Score: " + str(Globals.score)
+
+
+func _on_basket_body_entered(body: Node2D) -> void:
+	Globals.score = Globals.score + 1
