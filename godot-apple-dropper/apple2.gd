affbb3daf18c0a995e4c017d5b9f862d6632c90d
BellajArnold <bellajarnold@gmail.com>
2025-12-03 13:31:51 -0800

    apples disappear when in the basket

--- godot-apple-dropper/apple2.gd
+++ godot-apple-dropper/apple2.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var gravity = 30
 var score = 0
 
+
 func _ready():
 	velocity = Globals.apple_speed
 	
@@ -10,11 +11,16 @@ func _physics_process(delta):
 	move_and_slide()
 	if Globals.random == 1:
 		velocity.y += gravity
+		
 		if position.y > 1100:
 			position.y = 0
 			position.x = randi_range(0, 1500)
 			velocity.y = 200
 
-
-
+func _on_basket_body_entered(body: Node2D) -> void:
+	position.y = 0
+	position.x = randi_range(0, 1150)
+	velocity.y = 200
+	
+	
 	
