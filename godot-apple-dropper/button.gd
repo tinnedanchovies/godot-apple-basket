0a7725af9f1030ee8dc36565ccb5658f28c4b47d
tinnedanchovies <mollyvz009@gmail.com>
2025-12-04 15:03:59 -0800

    Added menu, finished start animation.

--- godot-apple-dropper/button.gd
+++ godot-apple-dropper/button.gd
@@ -1,6 +1,11 @@
 extends Button
 
-@onready var animation = get_node("/start_screen/AnimationPlayer/")
+@onready var playAnimation = get_node("/root/start_screen/AnimationPlayer/")
+@export var level = "res://level_1.tscn"
 
 func _on_pressed() -> void:
-	animation.play("apple_fall")
+	playAnimation.play("apple_fall")
+	
+
+func _on_animation_player_animation_finished(apple_fall) -> void:
+	get_tree().change_scene_to_file(level)
