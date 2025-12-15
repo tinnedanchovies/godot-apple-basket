1082c77e87bc50232c006c5381758ca6b0a953dc
tinnedanchovies <mollyvz009@gmail.com>
2025-12-15 13:47:49 -0800

    fixed buttons, fixed scoreboard

--- godot-apple-dropper/better_basket.gd
+++ godot-apple-dropper/better_basket.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-var score_board
+@export var scoreboard_node = ""
+@onready var score_board = get_node(scoreboard_node)
 
 func _ready():
 	position = Vector2(500, 550)
-	score_board = get_node("/root/level_1/score_board/score")
 
 func _process(delta):
 	move_and_slide()
@@ -14,20 +14,19 @@ func _process(delta):
 	if Input.is_key_pressed(KEY_LEFT):
 		position.x = position.x - 30
 
-#func _on_area_2d_2_body_entered(body: Node2D) -> void:
-	#if body.name == "bomb":
-		#print("it's a bomb")
-		#Globals.score = Globals.score - 5
-		#Globals.life_count = Globals.life_count + 1
-		#score_board.text = str(Globals.score)
-		#
-	#if body.name == "golden_apple":
-		#print("it's a golden apple")
-		#Globals.score = Globals.score + 5
-		#score_board.text = str(Globals.score)
-	#
-	#if body.name == "Apple":
-		#print("it's just an apple")
-		#Globals.score = Globals.score + 1
-		#score_board.text = str(Globals.score)
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if body.name == "bomb":
+		print("it's a bomb")
+		Globals.score = Globals.score - 5
+		Globals.life_count = Globals.life_count + 1
+		
+		
+	if body.name == "golden_apple":
+		print("it's a golden apple")
+		Globals.score = Globals.score + 5
+		
+	
+	if body.name == "Apple":
+		print("it's just an apple")
+		Globals.score = Globals.score + 1
 		
--- godot-apple-dropper/to_start.gd
+++ godot-apple-dropper/to_start.gd
@@ -3,4 +3,5 @@ extends Button
 func _on_pressed() -> void:
 	Globals.paused = false
 	Globals.score = 0
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://start_screen.tscn")
+	
