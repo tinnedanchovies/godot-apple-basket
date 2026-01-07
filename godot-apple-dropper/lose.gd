59feb642625e461fd4e05aa6211dcfe1a7bc998b
tinnedanchovies <mollyvz009@gmail.com>
2026-01-07 13:02:35 -0800

    fixed lose

--- godot-apple-dropper/lose.gd
+++ godot-apple-dropper/lose.gd
@@ -3,12 +3,10 @@ extends Control
 @export var menuNode = ""
 @onready var menu = get_node(menuNode)
 
-
 func _ready() -> void:
 	menu.hide()
 	
-
-func _process(delta):
+func _process(_delta):
 	if Globals.life_count == 0:
 		menu.show()
 	
